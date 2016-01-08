2d704bc819fbc094d6bcd2a75f40c78000bc5f27
Eric Summins <esummins@gmail.com>
2016-01-08 14:10:02 -0800

    added display

--- player.rb
+++ player.rb
@@ -4,11 +4,14 @@ class Player
     @name = name
   end
 
-  def display(board)
-
+  def display(board_state, width)
+    board_state.each_with_index do |spot, i|
+      print "/n" if i>0 && i % width ==0
+      print spot
+    end
   end
 
   def get_input
-    
+
   end
 end
